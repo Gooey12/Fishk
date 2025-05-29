3bc6bb58ec1a5e46bffba09951acf965e32133a9
Gooey12 <mrsimpleanimation@gmail.com>
2025-05-29 14:11:11 +0800

    Updated

--- vars.js
+++ vars.js
@@ -3,7 +3,7 @@ const variable = {
     Level: 0,
     XP: 0,
     FishInInventory: 0,
-    FishPrice: 7,
+    FishPrice: Math.round(Math.random() * (9 - 6) + 6),
     fishResetTimeout: null,
     sellResetTimeout: null,
     canFish: true
